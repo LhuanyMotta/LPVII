d25e1c0f84b8b6e8ebe465588e57e6c690dd4fbb
LhuanyMotta <lhuany.euzebio@hotmail.com>
2025-05-20 16:47:46 -0400

    Home modificado

--- AppConcurso/AppConcurso/obj/Debug/net8.0/AppConcurso.AssemblyInfo.cs
+++ AppConcurso/AppConcurso/obj/Debug/net8.0/AppConcurso.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppConcurso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1929515fafcec0ba6c88c95220b90d26589bb283")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppConcurso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppConcurso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
